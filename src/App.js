a3b505ee1cdd6db886d7a945beb790063b47ec8a
Ashley Rarieya <ararieya@gmail.com>
2023-11-10 09:50:58 +0300

    Added the app function

--- src/App.js
+++ src/App.js
@@ -6,3 +6,18 @@ import Comments from "./components/Comments";
 import Filter from "./components/Filter";
 import Search from "./components/Search";
 import NavBar from "./components/NavBar";
+
+function App() {
+  const [groceries, setGroceries] = useState([]);
+  const [selectedGroceries, setSelectedGroceries] = useState([]);
+  const [categoryFilter, setCategoryFilter] = useState('all');
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:3000/groceries")
+      .then((response) => response.json())
+      .then((data) => setGroceries(data))
+      .catch((error) => console.log(error));
+  }, []);
+  
+}
