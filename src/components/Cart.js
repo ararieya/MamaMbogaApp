010c5e4683a74e533e8cd1d7729a2fc59f627f6e
Abdulsalam Abdikarim <achalam00@gmail.com>
2023-11-10 11:34:22 +0300

    Cart items rendered

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,7 +4,22 @@ import Button from "react-bootstrap/Button";
 function Cart({selectedGroceries, removeFromYourCart}){
     return(
         <>
-         
+          <h2>Your Cart</h2>
+    <div className ='cart-container'>
+    {selectedGroceries.map((grocery) => (
+            <Card key={grocery.id} className='card-container' onClick={() => removeFromYourCart(grocery.id)}>
+            <Card.Img variant="top" src={grocery.image} alt={grocery.name} className='grocery-image' />
+            <Card.Body className="card-body">
+            <Card.Title>{grocery.category}</Card.Title>
+            <Card.Text className="card-text">
+              {grocery.name} <br/>
+              {`Price: ${grocery.price}`}
+            </Card.Text>
+            <Button variant="primary" onClick={() => removeFromYourCart(grocery)}> Remove from Cart</Button>{' '}
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
     
          </>
     )
