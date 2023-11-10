55d555e34d106b6953edba9790bf6987dccd3571
Joy <joyglory213@gmail.com>
2023-11-10 11:19:06 +0300

    Styling the Links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,1 +1,11 @@
 import { NavLink } from "react-router-dom";
+
+const linkStyles = {
+    display: "inline-block",
+    width: "200px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "none",
+    color: "white",
+  };
