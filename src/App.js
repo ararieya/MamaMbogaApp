f0bd0ea36de0a87d5d361b66c4eb4b1c85330c64
Ashley Rarieya <ararieya@gmail.com>
2023-11-10 09:49:05 +0300

    imported components to app.js

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React, {useState, useEffect} from "react";
+import {Routes, Route} from "react-router-dom"
+import Groceries from "./components/Groceries";
+import Cart from "./components/Cart";
+import Comments from "./components/Comments";
+import Filter from "./components/Filter";
+import Search from "./components/Search";
+import NavBar from "./components/NavBar";
