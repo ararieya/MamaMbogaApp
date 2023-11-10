0ca6300a2e09137d871a339f0a3447d301e6af7e
Abdulsalam Abdikarim <achalam00@gmail.com>
2023-11-10 11:31:46 +0300

    Cart Function created

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,2 +1,13 @@
 import { Card } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
+
+function Cart({selectedGroceries, removeFromYourCart}){
+    return(
+        <>
+         
+    
+         </>
+    )
+}
+
+export default Cart;
