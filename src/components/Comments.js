6e098c8952c515ed9f1b9b5c3b423b6b2758661e
Elijah Ngocho <elijahngocho24@gmail.com>
2023-11-11 10:33:01 +0300

    function comments declared

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -9,41 +9,41 @@ function Comments() {
          return;
         }
         
-        setComments([...comments, newComment]);
-        setNewComment('');
+     setComments([...comments, newComment]);
+     setNewComment('');
         };
         return (
             <>
-            <h2>Comments</h2>
-            <div className="container mt-5">
-            <div className="d-flex flex-row">
-            <div className="flex-grow-1 pr-3">
-            <div className="form-group mb-0">
-            <input
-            type="text"
-            className="form-control"
-            placeholder="Add a comment"
-            value={newComment}
-            onChange={(e) => setNewComment(e.target.value)}
-            />
+     <h2>Comments</h2>
+     <div className="container mt-5">
+     <div className="d-flex flex-row">
+      <div className="flex-grow-1 pr-3">
+      <div className="form-group mb-0">
+                <input
+                type="text"
+                className="form-control"
+                placeholder="Add a comment"
+                value={newComment}
+                onChange={(e) => setNewComment(e.target.value)}
+                />
             </div>
-</div>
-<div className="align-self-end">
-<button className="btn btn-primary" onClick={handleAddComment}>
-Add Comment
-</button>
-</div>
-</div>
-<div id = "comments" >
-<ul className="list-group mt-3">
-{comments.map((comment, index) => (
-<li key={index} className="list-group-item">
-{comment}
-</li>
+            </div>
+    <div className="align-self-end">
+    <button className="btn btn-primary" onClick={handleAddComment}>
+    Add Comment
+    </button>
+    </div>
+    </div>
+    <div id = "comments" >
+    <ul className="list-group mt-3">
+    {comments.map((comment, index) => (
+    <li key={index} className="list-group-item">
+    {comment}
+    </li>
 ))}
-</ul>
-</div>
-</div>
+    </ul>
+        </div>
+        </div>
 </>
 );
 }
