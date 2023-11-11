3c4929bcab0020253260689c08c02376e7f429fe
Elijah Ngocho <elijahngocho24@gmail.com>
2023-11-11 10:28:14 +0300

    comment function declared

---
+++ src/components/Comments.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+// import './index.css'; // Import your external CSS file
+
+function Comments() {
+    const [comments, setComments] = useState([]);
+    const [newComment, setNewComment] = useState('');
+    const handleAddComment = () => {
+        if (newComment.trim() === '') {
+         return;
+        }
+        
+        setComments([...comments, newComment]);
+        setNewComment('');
+        };
+        return (
+            <>
+            <h2>Comments</h2>
+            <div className="container mt-5">
+            <div className="d-flex flex-row">
+            <div className="flex-grow-1 pr-3">
+            <div className="form-group mb-0">
+            <input
+            type="text"
+            className="form-control"
+            placeholder="Add a comment"
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+            />
+            </div>
+</div>
+<div className="align-self-end">
+<button className="btn btn-primary" onClick={handleAddComment}>
+Add Comment
+</button>
+</div>
+</div>
+<div id = "comments" >
+<ul className="list-group mt-3">
+{comments.map((comment, index) => (
+<li key={index} className="list-group-item">
+{comment}
+</li>
+))}
+</ul>
+</div>
+</div>
+</>
+);
+}
+
+export default Comments;
